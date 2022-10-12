89223c47b3be340fd608811de21ffa77bdb5d79d
Chhit <chhit085@gmail.com>
2022-10-12 23:32:17 +0700

    my first project

---
+++ simple-web-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, temporibus?</p>
+    </>
+  );
+}
+
+export default App;
